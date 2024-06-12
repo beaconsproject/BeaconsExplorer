d08787e3b55913d6cb3f3836c0893a39b644727c
Melina Houle <houle.melina@gmail.com>
2024-06-11 22:12:18 -0400

    CReate modal window to upload extra features

---
+++ modules/data_modalUpload.R
@@ -0,0 +1,95 @@
+modalDialogUI <- function(id) {
+  ns <- NS(id)
+  tagList(
+    actionButton(ns("showModal"), "Upload additional features"),
+    uiOutput(ns("upload_line")),
+    uiOutput(ns("upload_poly"))
+  )
+}
+
+modalDialogServer <- function(input, output, session) {
+  ns <- session$ns
+  
+  observeEvent(input$showModal, {
+    showModal(modalDialog(
+      title = "Upload a File",
+      radioButtons(ns("lineType"), "Select File Type for linear features:",
+                   choices = list("Shapefile" = "shp", "GeoPackage" = "gpkg"),
+                   selected = "shp"),
+      uiOutput(ns("dynamicUploadLineUI")),
+      radioButtons(ns("polyType"), "Select File Type for polygon features:",
+                   choices = list("Shapefile" = "shp", "GeoPackage" = "gpkg"),
+                   selected = "shp"),
+      uiOutput(ns("dynamicUploadPolyUI")),
+      footer = tagList(
+        modalButton("Cancel"),
+        actionButton(ns("confirmUpload"), "Upload")
+      )
+    ))
+  })
+  
+  observeEvent(input$lineType, {
+    output$dynamicUploadLineUI <- renderUI({
+      if (input$lineType == "gpkg") {
+        gpkgUI(ns("upload_line"))
+      } else if (input$lineType == "shp") {
+        shpUI(ns("upload_line"))
+      }
+    })
+  })
+  
+  observeEvent(input$polyType, {
+    output$dynamicUploadPolyUI <- renderUI({
+      if (input$polyType == "gpkg") {
+        gpkgUI(ns("upload_poly"))
+      } else if (input$polyType == "shp") {
+        shpUI(ns("upload_poly"))
+      }
+    })
+  })
+  
+  upload_features <- reactiveValues(add_line = reactiveVal(NULL),
+                                    add_poly =reactiveVal(NULL))
+  #sa_data_line <- reactiveVal(NULL)
+ # sa_data_poly <- reactiveVal(NULL)
+  
+  observeEvent(input$confirmUpload, {
+    if (input$lineType == "gpkg") {
+      #sa_data_line(callModule(gpkg_upload, "upload_line"))
+      upload_features$add_line <- callModule(shp_upload, "upload_line")
+    } else if (input$lineType == "shp") {
+      #sa_data_line(callModule(shp_upload, "upload_line"))
+      upload_features$add_line <- callModule(shp_upload, "upload_line")
+    }
+    if (input$polyType == "gpkg") {
+      #sa_data_poly(callModule(gpkg_upload, "upload_poly"))
+      upload_features$add_poly <- callModule(shp_upload, "upload_poly")
+    } else if (input$polyType == "shp") {
+     # sa_data_poly(callModule(shp_upload, "upload_poly"))
+      upload_features$add_poly <- callModule(shp_upload, "upload_poly")
+    }
+    removeModal()
+  })
+  output$upload_line <- renderUI({
+    req(upload_features$add_line())
+    tagList(
+      verbatimTextOutput(ns("lineData"))
+    )
+  })
+  
+  output$upload_poly <- renderUI({
+    req(upload_features$add_poly())
+    tagList(
+      verbatimTextOutput(ns("polyData"))
+    )
+  })
+  
+  output$lineData <- renderPrint({
+    basename(sources(upload_features$add_line()))
+  })
+  
+  output$polyData <- renderPrint({
+    basename(sources(upload_features$add_poly()))
+  })
+  return(upload_features)
+}
